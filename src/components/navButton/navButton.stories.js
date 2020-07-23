ef3adba1013eaf64a3a732d328937e4e3befb650
jefiedler <jonas.fiedler@magenta.de>
2020-07-23 13:41:43 +0200

    Finisch bottom menu

--- src/components/navButton/navButton.stories.js
+++ src/components/navButton/navButton.stories.js
@@ -1,6 +1,6 @@
 import "./navButton.css";
 import navButton from "./navButton.html";
 
-export default { title: "List of Coffee Typs" };
+export default { title: "Bottom Button" };
 
 export const navbarButton = () => navButton;
