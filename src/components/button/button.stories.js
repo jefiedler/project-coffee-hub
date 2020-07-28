d2f5b915a9f82c29e25994df718c78e2733513d7
jefiedler <jonas.fiedler@magenta.de>
2020-07-28 14:31:15 +0200

    Add create button function

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,16 @@
 import "./button.css";
-import button from "./button.html";
 
 export default { title: "Button" };
 
-export const addToCart = () => button;
+function createButton(buttonText) {
+  const button = document.createElement("button");
+  button.classList.add("btn");
+  const text = document.createTextNode(buttonText);
+  button.append(text);
+  return button;
+}
+
+export const addToCart = () => {
+  const addToCartButton = createButton("Add to cart");
+  return addToCartButton;
+};
