38e9d5fe67c2bdad6ec5dd8ce5db9130d38bcc06
jefiedler <jonas.fiedler@magenta.de>
2020-07-28 16:45:03 +0200

    Outsurce button function in button.js

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,14 +1,7 @@
 import "./button.css";
+import { createButton } from "./button";
 
 export default { title: "Button" };
-
-function createButton(buttonText) {
-  const button = document.createElement("button");
-  button.classList.add("btn");
-  const text = document.createTextNode(buttonText);
-  button.append(text);
-  return button;
-}
 
 export const addToCart = () => {
   const addToCartButton = createButton("Add to cart");
